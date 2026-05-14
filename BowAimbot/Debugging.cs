4b2850cc19d314ed7f5fa8fcae9a140587094e4a
Nightly <177694366+TheNightlyLol@users.noreply.github.com>
2026-05-14 17:21:11 -0400

    Made Debugging.cs with a foreach loop OnItemEnterTrigger as an attempt to grab the ids of the items that the CrystalMusket fires.

---
+++ BowAimbot/Debugging.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine; 
+using ThunderRoad;
+
+namespace BasAimbot
+{
+    public class Debugging : ThunderScript
+    {
+        private RagdollHand hand;
+
+        public override void ScriptLoaded(ModManager.ModData modData)
+        {
+            base.ScriptLoaded(modData);
+            EventManager.onItemEnterTrigger += OnItemEnterTrigger;
+        }
+
+        private void OnItemEnterTrigger(Item item, string triggerTag)
+        {
+            if (item?.data?.id != "CrystalMusket") return;
+            if (hand.creature != Player.currentCreature) return; 
+            foreach (Item i in Item.allActive)
+            {
+                Debug.Log($"Item ID's: {i.data.id} and this is the triggerTag to see what it is: {triggerTag}");
+            }
+        }
+    }
+}
